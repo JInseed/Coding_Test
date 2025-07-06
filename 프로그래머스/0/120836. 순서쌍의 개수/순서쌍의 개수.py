1955d1ae938f2a710b00306f0db9e44e3342a984
JInseed <120428959+JInseed@users.noreply.github.com>
2025-07-06 22:33:44 +0900

    [level 0] Title: 순서쌍의 개수, Time: 97.27 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -5,3 +5,14 @@ def solution(n):
             a.append(i)
             
     return len(a)
+
+# def solution(n):
+#     answer = 0
+#     for i in range(1, int(n ** 0.5) + 1):
+#         if n % i == 0:
+#             answer += 2
+
+#             if i * i == n:
+#                 answer -= 1
+
+#     return answer
