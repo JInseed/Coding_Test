3c5b5e7776ac32e70fdd888752d2b516614784f3
JInseed <120428959+JInseed@users.noreply.github.com>
2025-07-08 23:38:58 +0900

    [level 0] Title: 약수 구하기, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = []
-    for i in range(1, int(n**(1/2))+1):
+    for i in range(1, int(n**(0.5))+1):
         if (n%i == 0):
             answer+=list(set([i, n//i]))
     return sorted(answer)
