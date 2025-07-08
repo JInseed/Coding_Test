ae67c3140fa13ff05eae70c1d11bd10492342422
JInseed <120428959+JInseed@users.noreply.github.com>
2025-07-08 22:14:40 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,4 +1,6 @@
+# def solution(cipher, code):
+#     answer = cipher[code-1::code]
+#     return answer
+
 def solution(cipher, code):
-    answer = cipher[code-1::code]
-    return answer
-    
+    return ''.join(cipher[i] for i in range(code-1,len(cipher),code))
