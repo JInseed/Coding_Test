48a55c751adfa657e4056356094c28837b09add9
JInseed <120428959+JInseed@users.noreply.github.com>
2025-07-10 21:58:54 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.12 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,2 +1,5 @@
 def solution(my_string, index_list):
     return ''.join(my_string[i] for i in index_list)
+
+# def solution(my_string, index_list):
+#     return ''.join(map(lambda x: my_string[x], index_list))
