37a0b92e8e8be98c310884919c7601bcdf5e3d5a
JInseed <120428959+JInseed@users.noreply.github.com>
2025-07-11 20:44:32 +0900

    [level 0] Title: 수 조작하기 2, Time: 35.67 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -10,3 +10,10 @@ def solution(numLog):
         else:
             answer+='a'
     return answer
+
+# def solution(log):
+#     res=''
+#     joystick=dict(zip([1,-1,10,-10],['w','s','d','a']))
+#     for i in range(1,len(log)):
+#         res+=joystick[log[i]-log[i-1]]
+#     return res
